9e3745620c03f9114c09f38b7904eb2d082bb8f6
deepak19d <deepak19ece@gmail.com>
2023-03-31 17:33:19 +0530

    Home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import '@/styles/globals.css'
+import '@/styles/bootstrap.min.css'
+import '@/styles/templatemo.css'
+
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
