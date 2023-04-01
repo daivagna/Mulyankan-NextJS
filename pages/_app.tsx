edc449df87b93039d4aee6021ca56980fd5d3537
Nisheesh Kulshreshtha <con_nkulshreshtha@plancanada.ca>
2023-04-01 12:39:08 +0530

    hope page carousel compoent is now ready!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import '@/styles/globals.css'
 import '@/styles/bootstrap.min.css'
 import '@/styles/templatemo.css'
 
-
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <>
+        <Script src="/scripts/bootstrap.bundle.min.js" />
+        <Script src="/scripts/templatemo.js" />
+        <Script src="/scripts/custom.js" />
+        <Component {...pageProps} />
+        </>
 }
