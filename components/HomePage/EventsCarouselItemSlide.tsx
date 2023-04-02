6b94f94021f8bbb23ecfac3c195e105a263f8e32
Nisheesh Kulshreshtha <con_nkulshreshtha@plancanada.ca>
2023-04-02 16:06:53 +0530

    no message

--- components/HomePage/EventsCarouselItemSlide.tsx
+++
@@ -1,9 +0,0 @@
-
-const EventsCarouselItemSlide = (props:any) => {
-    return <>
-        {props.events.map((event:any, index:any) => (
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to={(index)} className={(index == 0 ? 'active' : '')} key={index}></li>            
-        ))}
-    </>
-}
-export default EventsCarouselItemSlide;
