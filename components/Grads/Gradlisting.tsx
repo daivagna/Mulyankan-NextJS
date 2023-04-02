adc22c54a3395f9ecda47367c321e6fe03008cec
Nisheesh Kulshreshtha <con_nkulshreshtha@plancanada.ca>
2023-04-02 16:14:44 +0530

    commit

--- components/Grads/Gradlisting.tsx
+++ components/Grads/Gradlisting.tsx
@@ -4,60 +4,63 @@ import Pagination from "./GradsPagination";
 import { useState, useEffect, Dispatch, SetStateAction } from "react";
 
 type PageProps = {
-  page: any;
- 
+    page: any;
+
 };
 
 export interface GradlistingProps {
-  selectedCategory: SetStateAction<string>;
+    selectedCategory: SetStateAction<string>;
 }
 
 const paginate = (items: any, pageNumber: any, pageSize: any) => {
-  const startIndex = (pageNumber - 1) * pageSize;
-  return items.slice(startIndex, startIndex + pageSize);
+    const startIndex = (pageNumber - 1) * pageSize;
+    return items.slice(startIndex, startIndex + pageSize);
 };
 
 const GradCardData = (gradsnewData: any) => {
-  return <GradCard grads={gradsnewData}></GradCard>;
+    return <GradCard grads={gradsnewData}></GradCard>;
 };
 
 const Gradlisting: React.FC<GradlistingProps> = (props: any) => {
-  const [currentPage, setCurrentPage] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
 
-  const pageSize = 5;
-  let GradsFilterdData = GradsData;
-  // if (props.selectedCategory != "") {
-  //   let filters = props.selectedCategory.split("##");
-  //   if (filters[1] == "interestedin")
-  //     GradsFilterdData = GradsData.filter(
-  //       (grad) => grad.interestedin == filters[0]
-  //     );
-  // }
+    const pageSize = 6;
+    let GradsFilterdData = GradsData;
+    // if (props.selectedCategory != "") {
+    //   let filters = props.selectedCategory.split("##");
+    //   if (filters[1] == "interestedin")
+    //     GradsFilterdData = GradsData.filter(
+    //       (grad) => grad.interestedin == filters[0]
+    //     );
+    // }
 
-  let gradsnewData = paginate(GradsFilterdData, 1, pageSize);
-  const [gradsFilterData, setGradsData] = useState(gradsnewData);
+    let gradsnewData = paginate(GradsFilterdData, 1, pageSize);
+    const [gradsFilterData, setGradsData] = useState(gradsnewData);
 
-    
-  const onPageChangeNew: React.FunctionComponent<PageProps>  = (props)  => {
-    setCurrentPage(props.page);
-    gradsnewData = paginate(GradsFilterdData, props.page, pageSize);
-    setGradsData(gradsnewData);
-    return null;
-  };
 
-  return (
-    <div className="col-lg-9">
-      <div className="row">{GradCardData(gradsFilterData)}</div>
-      <div className="row">
-        <Pagination
-          items={GradsData.length} // 100
-          currentPage={currentPage} // 1
-          pageSize={pageSize} // 10
-          onPageChangeNew={onPageChangeNew}
-        />
-      </div>
-    </div>
-  );
+    const onPageChangeNew: React.FunctionComponent<PageProps> = (props: any) => {
+        setCurrentPage(props);
+        gradsnewData = paginate(GradsFilterdData, props, pageSize);
+        setGradsData(gradsnewData);
+        return null;
+    };
+
+    return (
+        <>
+            <h1>Grads</h1>
+            <div className="col-lg-12">
+                <div className="row">{GradCardData(gradsFilterData)}</div>
+                <div className="row">
+                    <Pagination
+                        items={GradsData.length} // 100
+                        currentPage={currentPage} // 1
+                        pageSize={pageSize} // 10
+                        onPageChangeNew={onPageChangeNew}
+                    />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Gradlisting;
