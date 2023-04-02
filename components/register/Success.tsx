b292d25727f0607923c6ee533df83c0d5f28eb91
Akshay Madhukar Barve <abarve@horizontal.com>
2023-04-02 13:07:43 +0530

    Email Sending Code + Success Page after registration

---
+++ components/register/Success.tsx
@@ -0,0 +1,18 @@
+const Success = () => {
+  return (
+    <section className="bg-success py-5">
+        <div className="container">
+            <div className="row align-items-center py-5">
+                <div className="col-md-8 text-white">
+                    <h1>Registration Successfull</h1>
+                    <p>
+                    Thank your registration on Grad & Training Internship Program, this page is a receipt of your registration, You should have received an email in your registered email !!! In case of any questions or concerns feel free to contact us on gradsprogram@horizontal.com
+                    </p>
+                </div>
+               
+            </div>
+        </div>
+    </section>
+  );
+};
+export default Success;
